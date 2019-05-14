11731d9e385dab0def56260fd2c4929c901ce78a
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-05-14 13:12:35 +0700

    Footer centerized.

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -16,7 +16,7 @@
     </div>
 
     <footer class="bg-dark text-light py-3">
-        <div class="container">
+        <div class="container text-center">
             Copyright {{ today()->format('Y') }} © Konservasi Hutan Mangrove
         </div>
     </footer>
